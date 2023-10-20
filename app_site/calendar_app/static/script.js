99502515eb7d1d3ad4ceea62e7b3f9fbf7cd418f
aver <froloff92@mail.ru>
2023-10-20 17:14:57 +0300

    fixed js checkbox

--- app_site/calendar_app/static/script.js
+++ app_site/calendar_app/static/script.js
@@ -1,5 +1,7 @@
 const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-function create_calendar(fixed_days){
+function create_calendar(params){
+    fixed_days = Object.keys(params);
+    days_colors = Object.values(params);
 
     // get current date values
     const currentDate = new Date();
@@ -16,16 +18,17 @@ function create_calendar(fixed_days){
     let week = document.createElement('div');
     week.classList.add('calendar__day-numbers-row');
 
-
-
     for (i = 1; i <= daysInMonth; i++) {
         let day = document.createElement('span');
         day.classList.add('calendar__day-number');
         day.innerText = i;
         (i == currentDay) && day.classList.add('calendar__day-number--current');
         week.append(day);
-        if (fixed_days.includes(i)){
-            day.setAttribute("day_color", "red")
+        if (fixed_days.includes(String(i))){
+            const find_colors_re = /rgb\(\d+, \d+, \d+\)/g;
+            gradient_colors = params[String(i)].match(find_colors_re);
+            day.style.backgroundImage = getSplitBackgroundStyle(gradient_colors);
+            day.setAttribute("day_color", params[String(i)]);
         }
 
         if (new Date(currentYear, currentMonth, i).getDay() == 0 || i == daysInMonth) {
@@ -39,12 +42,14 @@ function create_calendar(fixed_days){
     }
 
 
-    const colors = ["red","green","blue"];
+    const colors = ['rgb(255, 0, 0)', 'rgb(0, 255, 0)'];
+    checkbox_create(colors);
+}
+
+function checkbox_create(colors){
     colors.forEach((color)=>{
         //  generate id
         const id = `color-${color}`;
-
-        // create a label
         const label = document.createElement('label');
         label.setAttribute("for", id);
 
@@ -54,35 +59,55 @@ function create_calendar(fixed_days){
         checkbox.name = "color";
         checkbox.value = color;
         checkbox.id = id;
+        checkbox.setAttribute("checked", true);
         checkbox.classList.add('calendar__checkbox')
 
         checkbox.addEventListener('change', function() {
+            var color_filter = '.calendar__day-number[day_color*="' + color + '"]'
+            var color_days = document.querySelectorAll(color_filter);
+            const find_colors_re = /rgb\(\d+, \d+, \d+\)/g;
             if (this.checked) {
                 console.log("Checkbox is checked..");
-                var color_filter = '.calendar__day-number[day_color=' + color + ']'
-                var color_days = document.querySelectorAll(color_filter);
                 color_days.forEach((color_day)=>{
-                    color_day.style.background = color;
+                    var gradient_colors = [];
+                    gradient_colors = color_day.style.backgroundImage.match(find_colors_re);
+                    gradient_colors = removeDuplicates(gradient_colors);
+                    gradient_colors.push(color);
+
+                    color_day.style.backgroundImage = getSplitBackgroundStyle(gradient_colors);
                 });
             }
             else {
-                var empty_style = document.querySelector('.calendar__day-number:not([day_color])').style
-                var color_filter = '.calendar__day-number[day_color=' + color + ']'
-                var color_days = document.querySelectorAll(color_filter);
-                color_days.forEach((color_day)=>{
-                    color_day.style = empty_style;
-                });
                 console.log("Checkbox is not checked..");
-            }
+                color_days.forEach((color_day)=>{
+                    gradient_colors = color_day.style.backgroundImage.match(find_colors_re);
+                    gradient_colors = gradient_colors.filter(function(e) { return e !== color });
+                    gradient_colors = removeDuplicates(gradient_colors);
+                    color_day.style.backgroundImage = getSplitBackgroundStyle(gradient_colors);
+                });
+            };
         });
-
-        // place the checkbox inside a label
-        label.appendChild(checkbox);
-        // create text node
-        label.appendChild(document.createTextNode(color));
-        // add the label to the root
-        br = document.createElement("br");
-        document.querySelector("#root").appendChild(label).appendChild(br);
+    // place the checkbox inside a label
+    label.appendChild(checkbox);
+    // create text node
+    label.appendChild(document.createTextNode(color));
+    // add the label to the root
+    br = document.createElement("br");
+    document.querySelector("#root").appendChild(label).appendChild(br);
     });
+}
+
+
+function getSplitBackgroundStyle(colors, angle = "to bottom"){
+    let backgroundStyle = 'unset'
+    if (colors.length) {
+        const percent = Math.floor(100 / colors.length)
+        backgroundStyle = `linear-gradient(${angle}, ${colors.map((color, index) => `${color} ${index * percent}% ${(index + 1) * percent}%`).join(', ')})`
+    }
+    return backgroundStyle;
+}
+
+function removeDuplicates(arr) {
+    return [...new Set(arr)];
 }
 
--- app_site/calendar_app/views.py
+++ app_site/calendar_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-
+import json
 # Create your views here.
 
 
 def index(request):
-    fixed_days = [1, 5, 6, 8, 13, 18, 20, 24, 25, 29]
+    fixed_days = [1, 5, 6, 8, 13, 18, 19, 24, 25, 29]
+    colors = ['rgb(255, 0, 0), rgb(0, 255, 0)', "rgb(255, 0, 0), rgb(255, 0, 0)", "rgb(255, 0, 0), rgb(0, 255, 0)", "rgb(255, 0, 0), rgb(255, 0, 0)", "rgb(255, 0, 0), rgb(255, 0, 0)",
+              "rgb(0, 255, 0), rgb(0, 255, 0)", "rgb(0, 255, 0), rgb(0, 255, 0)", "rgb(255, 0, 0), rgb(0, 255, 0)", "rgb(0, 255, 0), rgb(0, 255, 0)", "rgb(0, 255, 0), rgb(0, 255, 0)"]
+    params = dict(zip(fixed_days, colors))
     context = {
-        "days": fixed_days
+        "params": json.dumps(params),
     }
     return render(request, "index.html", context)
